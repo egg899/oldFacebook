9b436d37ffd166ff869d0dabf6f779147e4ec2b6
Alberto German Verissimo <germanverissimo5@hotmail.com>
2026-07-06 02:49:55 -0300

    input aderido

--- src/components/FeedFacebook.vue
+++ src/components/FeedFacebook.vue
@@ -91,6 +91,12 @@ import usuario from "../data/usuario.js";
             <input
                 type="file"
                 accept="image/*"
+                class="block w-full text-sm text-gray-500
+                file:mr-4 file:py-2 file:px-4
+                file:rounded-md file:border-0
+                file:text-sm file:font-semibold
+                file:bg-indigo-50 file:text-indigo-700
+                hover:file:bg-indigo-100 "
                 @change="seleccionarImagen"
             />
             
